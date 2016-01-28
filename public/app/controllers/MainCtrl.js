ef965e14f62e1020c21a25539e2e68b74fd413ac
you@example.com <you@example.com>
2016-01-28 13:04:09 -0430

    Sign Up added

--- public/app/controllers/MainCtrl.js
+++ public/app/controllers/MainCtrl.js
@@ -1,75 +1,86 @@
-var app = angular.module('LoginApp', ['ui.router','satellizer']);
-app.config(function($stateProvider, $urlRouterProvider,$authProvider) {
-//routes config
-$stateProvider
-      .state('home', {
-        url: '/',
-        controller: 'LoginCtrl',
-        templateUrl: 'app/views/home.html'
-    });
+var app = angular.module('LoginApp', ['ui.router', 'satellizer']);
+app.config(function($stateProvider, $urlRouterProvider, $authProvider) {
+    //routes config
+    $stateProvider
+        .state('home', {
+            url: '/',
+            controller: 'HomeCtrl',
+            templateUrl: 'app/views/home.html'
+        })
+        .state('login', {
+            url: '/login',
+            controller: 'LoginCtrl',
+            templateUrl: 'app/views/login.html'
+        })
+        .state('signup', {
+            url: '/signup',
+            controller: 'SignupCtrl',
+            templateUrl: 'app/views/signup.html'
+        });
+
     $urlRouterProvider.otherwise('/');
 
-//satellizer config
-  $authProvider.httpInterceptor = true;
-  $authProvider.tokenPrefix = "loginhub";
-  $authProvider.facebook({
-    clientId: '541848339317068'
-  });
-  $authProvider.twitter({
-    url: '/auth/twitter'
-  });
+    //satellizer config
+    $authProvider.httpInterceptor = true;
+    $authProvider.tokenPrefix = "loginhub";
+    $authProvider.facebook({
+        clientId: '541848339317068'
+    });
+    $authProvider.twitter({
+        url: '/auth/twitter'
+    });
 });
 
 app.controller('LoginCtrl', function($scope, $auth, $location, Account) {
-  $scope.authenticate = function(provider) {
-    $auth.authenticate(provider)
-      .then(function(data) {
-        console.log(data);
-        $location.path('/profile');
-      })
-      .catch(function(error) {
-        if (error) {
-          console.log(error);
-        }
-      });
-  };
-  $scope.getProfile = function() {
-    Account.getProfile()
-      .then(function(response) {
-        $scope.user = response.data;
-        console.log(response.data);
-      })
-      .catch(function(response) {
+    $scope.authenticate = function(provider) {
+        $auth.authenticate(provider)
+            .then(function(data) {
+                console.log(data);
+                $location.path('/profile');
+            })
+            .catch(function(error) {
+                if (error) {
+                    console.log(error);
+                }
+            });
+    };
+    $scope.getProfile = function() {
+        Account.getProfile()
+            .then(function(response) {
+                $scope.user = response.data;
+                console.log(response.data);
+            })
+            .catch(function(response) {
 
-      });
-  };
-  $scope.updateProfile = function() {
-    Account.updateProfile($scope.user)
-      .then(function() {
+            });
+    };
+    $scope.updateProfile = function() {
+        Account.updateProfile($scope.user)
+            .then(function() {
 
-      })
-      .catch(function(response) {
+            })
+            .catch(function(response) {
 
-      });
-  };
-  $scope.link = function(provider) {
-    $auth.link(provider)
-      .then(function() {
+            });
+    };
+    $scope.link = function(provider) {
+        $auth.link(provider)
+            .then(function() {
 
-        $scope.getProfile();
-      })
-      .catch(function(response) {
+                $scope.getProfile();
+            })
+            .catch(function(response) {
 
-      });
-  };
-  $scope.unlink = function(provider) {
-    $auth.unlink(provider)
-      .then(function() {
+            });
+    };
+    $scope.unlink = function(provider) {
+        $auth.unlink(provider)
+            .then(function() {
 
-        $scope.getProfile();
-      })
-      .catch(function(response) {
+                $scope.getProfile();
+            })
+            .catch(function(response) {
 
-      });
-  };
+            });
+    };
 });
