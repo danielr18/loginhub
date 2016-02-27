9f85951a927f3637124523e3740dc19866619cd7
unknown <@danielreinoso1807@gmail.com>
2016-02-27 10:11:33 -0430

    Validate email availability if passes pattern

--- public/app/controllers/signup.js
+++ public/app/controllers/signup.js
@@ -17,6 +17,4 @@ app.controller('SignupCtrl', function($scope, $location, $auth) {
           }
       });
   };
-  $scope.emailRegex = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i;
-  //Taken from http://emailregex.com/
 });
--- public/app/directives/validateEmail.js
+++ public/app/directives/validateEmail.js
@@ -11,17 +11,25 @@ app.directive('validateEmail', function($http) {
     // create linking function and pass in our NgModelController as a 4th argument
     link: function(scope, element, attr, ctrl) {
 
+      var emailRegex = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i;
+      //Taken from http://emailregex.com/
       // please note you can name your function & argument anything you like
       function customValidator(ngModelValue) {
-        $http.post('/auth/email_available', {email: ngModelValue})
-        .then(function(res) {
-          if(res.data.taken) {
-            ctrl.$setValidity('taken', false);
-          }
-          else {
-            ctrl.$setValidity('taken', true);
-          }
-        });
+        if(emailRegex.test(ngModelValue)) {
+          ctrl.$setValidity('pattern', true);
+          $http.post('/auth/email_available', {email: ngModelValue})
+          .then(function(res) {
+            if(res.data.taken) {
+              ctrl.$setValidity('taken', false);
+            }
+            else {
+              ctrl.$setValidity('taken', true);
+            }
+          });
+        }
+        else {
+          ctrl.$setValidity('pattern', false);
+        }
         // we need to return our ngModelValue, to be displayed to the user(value of the input)
         return ngModelValue;
       }
